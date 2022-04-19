413a6339bf874fa8aa9347c88ccef861ecbd20cc
reallyhategithub <castleracer@outlook.com>
2022-04-19 12:05:14 -0500

    about to experiment rewriting some of ShipFactory's functions in Gameboard

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,12 +1,1 @@
 import { ShipFactory } from "./ShipFactory";
-
-// export function Gameboard(name) {
-//   const { setCoordinates, getCoordinates } = new Ship(name);
-//   const saySomethingCool = () => console.debug("Something Cool");
-//   return { setCoordinates, getCoordinates, saySomethingCool };
-// }
-
-// export function Gameboard(){
-
-//     ShipFactory
-// }
